8e74cad0413bebb2678009cf9e154ed410437050
tianzz05 <tianzz05@163.com>
2017-01-04 16:21:23 +0800

    ComboBox-Style

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Commands;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
@@ -14,8 +15,12 @@ namespace WPFDemo
         {
             InitializeComponent();
 
-            TestCommand = new DelegateCommand(this.OnTest);
+            combobox.ItemsSource = list;
         }
+        List<string> list = new List<string> { "1", "2", "3" };
+
+
+
 
         public ICommand TestCommand { get; set; }
         void OnTest()
@@ -23,65 +28,18 @@ namespace WPFDemo
             MessageBox.Show("Test OK!");
         }
 
-        private Storyboard VisibilitySB
-        {
-            get
-            {
-                return this.FindResource("SB_Visibility") as Storyboard;
-            }
-        }
-
-        private Storyboard CollapsedSB
-        {
-            get
-            {
-                return this.FindResource("SB_Collapsed") as Storyboard;
-            }
-        }
-
-
-        bool flag = false;
 
         private void ImageRadioButton_Click(object sender, RoutedEventArgs e)
         {
-            //折叠
-            if (flag)
-            {
-                flag = false;
-                if (this.stackpanel.Visibility != Visibility.Collapsed)
-                {
-                    this.stackpanel.Visibility = Visibility.Collapsed;
-                }
-            }
-            else//展开
-            {
-                flag = true;
-                if (this.stackpanel.Visibility != Visibility.Visible)
-                {
-                    this.stackpanel.Visibility = Visibility.Visible;
-                }
-            }
+           
         }
 
         private void ImageRadioButton_Unchecked(object sender, RoutedEventArgs e)
         {
             this.stackpanel.Visibility = Visibility.Collapsed;
-            flag = false;
-        }
-
-        bool check = false;
-        private void ImageRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (!check)
-            {
-
-                this.stackpanel.Visibility = Visibility.Visible;
-                //flag = true;
-
-                check = true;
-            }
-
 
         }
+
+
     }
 }
